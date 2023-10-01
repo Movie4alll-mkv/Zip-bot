1c05ef45eca1dc52e0c72fbb5e42dd0cbe2c14d4
Movie4alll-mkv <77849768+Movie4alll-mkv@users.noreply.github.com>
2023-10-01 20:19:48 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,26 +1,78 @@
 import pyrogram
-from zipfile import ZipFile
-from pyrogram.types import Message
-class ZipBot(pyrogram.Client):
+import zipfile
+import rarfile
+
+class ZipRarBot(pyrogram.Client):
     async def zip(self, message: pyrogram.Message):
-        """Zips the source code from GitHub and sends it to the user."""
+        """Zips the file sent by the user and sends it back to them."""
 
-        # Get the GitHub repository URL from the user.
-        github_repo_url = message.text.split()[1]
+        # Get the file path from the user.
+        file_path = message.document.file_path
 
-        # Download the source code from GitHub.
-        source_code_file_path = await self.download_file(github_repo_url)
-
-        # Create a zip file of the source code.
-        zip_file_path = source_code_file_path + ".zip"
-        with ZipFile(zip_file_path, "w") as zip_file:
-            zip_file.write(source_code_file_path)
+        # Create a zip file of the file.
+        zip_file_path = file_path + ".zip"
+        with zipfile.ZipFile(zip_file_path, "w") as zip_file:
+            zip_file.write(file_path)
 
         # Send the zip file to the user.
         await self.send_document(message.chat.id, zip_file_path)
 
+    async def rar(self, message: pyrogram.Message):
+        """Rars the file sent by the user and sends it back to them."""
+
+        # Get the file path from the user.
+        file_path = message.document.file_path
+
+        # Create a rar file of the file.
+        rar_file_path = file_path + ".rar"
+        with rarfile.RarFile(rar_file_path, "w") as rar_file:
+            rar_file.write(file_path)
+
+        # Send the rar file to the user.
+        await self.send_document(message.chat.id, rar_file_path)
+
+    async def zip_with_password(self, message: pyrogram.Message):
+        """Zips the file sent by the user and sends it back to them with a password."""
+
+        # Get the file path from the user.
+        file_path = message.document.file_path
+
+        # Get the password from the user.
+        password = message.text.split()[1]
+
+        # Create a zip file of the file with a password.
+        zip_file_path = file_path + ".zip"
+        with zipfile.ZipFile(zip_file_path, "w", zipfile.ZIP_DEFLATED) as zip_file:
+            zip_file.write(file_path, password)
+
+        # Send the zip file to the user.
+        await self.send_document(message.chat.id, zip_file_path)
+
+    async def rar_with_password(self, message: pyrogram.Message):
+        """Rars the file sent by the user and sends it back to them with a password."""
+
+        # Get the file path from the user.
+        file_path = message.document.file_path
+
+        # Get the password from the user.
+        password = message.text.split()[1]
+
+        # Create a rar file of the file with a password.
+        rar_file_path = file_path + ".rar"
+        with rarfile.RarFile(rar_file_path, "w", mode="w") as rar_file:
+            rar_file.write(file_path, password)
+
+        # Send the rar file to the user.
+        await self.send_document(message.chat.id, rar_file_path)
+
 # Create a new bot.
-bot = ZipBot("5053402593:AAGDN6XuOz4qKWgPIatEGfkJqoporK94h78")
+bot = ZipRarBot("YOUR_BOT_API_TOKEN")
+
+# Add handlers for the /zip, /rar, /zip_with_password, and /rar_with_password commands.
+bot.add_handler(filters.command("zip") & filters.document, zip)
+bot.add_handler(filters.command("rar") & filters.document, rar)
+bot.add_handler(filters.command("zip_with_password") & filters.document, zip_with_password)
+bot.add_handler(filters.command("rar_with_password") & filters.document, rar_with_password)
 
 # Start the bot.
 bot.run()
