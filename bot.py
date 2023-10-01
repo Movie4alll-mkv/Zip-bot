d9b49774d33564ca151327dbf5caceea8d5a56c2
Movie4alll-mkv <77849768+Movie4alll-mkv@users.noreply.github.com>
2023-10-01 21:45:00 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -71,6 +71,13 @@ class ZipRarBot(pyrogram.Client):
 # Create a new bot.
 bot = ZipRarBot("5053402593:AAGDN6XuOz4qKWgPIatEGfkJqoporK94h78")
 
+# Replace the following values with your own API ID and API hash
+API_ID = 6534707
+API_HASH = "4bcc61d959a9f403b2f20149cbbe627a"
+
+# Create a new Pyrogram client
+client = pyrogram.Client("bot", api_id=API_ID, api_hash=API_HASH)
+
 # Add handlers for the /zip, /rar, /zip_with_password, and /rar_with_password commands.
 bot.add_handler(filters.command("zip") & filters.document, zip)
 bot.add_handler(filters.command("zip_with_password") & filters.document, zip_with_password)
